c7f04c29a5b58e2d79a6be6d2e9b6f88a49a9107
Hanhee Song <song.hanhee@gmail.com>
2018-01-23 21:27:38 -0500

    add files to drum class

--- src/classes/drums.js
+++ src/classes/drums.js
@@ -3,10 +3,20 @@ import Instrument from './instrument';
 class Drums extends Instrument {
   constructor(props) {
     super(props);
+    this.setup();
+    this.initializeMarble();
   }
   
   setup() {
-    const notes = ["kick", "snare", "bass"];
+    const notes = ["kick", "snare", "bass", "crash"];
+    notes.forEach((note) => {
+      this.sounds[note] = new Audio(`audio/drums_${note}.wav`);
+      this.sounds[note].url = `public/audio/drums_${note}.wav`;
+    });
+  }
+  
+  initializeMarble() {
+    
   }
 }
 
