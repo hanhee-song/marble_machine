aa7458193214c4cfecc0e3b7c2b8c91232bbf0d4
Hanhee Song <song.hanhee@gmail.com>
2018-01-24 02:18:23 -0500

    fix drum pattern

--- src/classes/drums.js
+++ src/classes/drums.js
@@ -16,9 +16,14 @@ class Drums extends Instrument {
   }
   
   initializeMarble() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 12; i++) {
       this.addNote("snare", 8 * i + 4);
     }
+    this.addNote("snare", 104);
+    this.addNote("snare", 110);
+    this.addNote("snare", 114);
+    this.addNote("snare", 120);
+    
     for (let i = 0; i < 16; i++) {
       this.addNote("kick", 8 * i);
     }
