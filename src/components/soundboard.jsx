a1d10b3eff010f4a4fcef8ce405b79515ac9b021
Hanhee Song <song.hanhee@gmail.com>
2018-01-24 16:15:34 -0500

    add pause button and functionality to soundboard, slight restructure of html

--- src/components/soundboard.jsx
+++ src/components/soundboard.jsx
@@ -11,24 +11,46 @@ class Soundboard extends React.Component {
       tempo: 105,
       mm: 128,
     };
+    this.handlePause = this.handlePause.bind(this);
+    this.handleResume = this.handleResume.bind(this);
+    this.toggleRunning = this.toggleRunning.bind(this);
+  }
+    
+  handleResume() {
+    const interval = setInterval(() => {
+      const nextBeat = this.state.currentBeat + 1 === this.state.mm
+      ? 0 : this.state.currentBeat + 1;
+      this.setState({ currentBeat: nextBeat });
+    }, this.state.tempo);
+    this.setState({ interval });
   }
   
-  componentDidMount() {
-    setTimeout(() => {
-      const interval = setInterval(() => {
-        const nextBeat = this.state.currentBeat + 1 === this.state.mm
-            ? 0 : this.state.currentBeat + 1;
-        this.setState({ currentBeat: nextBeat });
-      }, this.state.tempo);
-      this.setState({ interval });
-    }, 3000);
+  handlePause() {
+    clearInterval(this.state.interval);
+    this.setState({ interval: null });
+  }
+  
+  toggleRunning() {
+    if (this.state.interval) {
+      this.handlePause();
+    } else {
+      this.handleResume();
+    }
   }
   
   render () {
+    const startButton = this.state.interval ? "pause" : "play";
     return (
       <div className="soundboard">
-        <VibraphoneBoard currentBeat={this.state.currentBeat} mm={this.state.mm} />
-        <DrumsBoard currentBeat={this.state.currentBeat} mm={this.state.mm} />
+        <div className="soundboard-controls">
+          <button className="soundboard-controls-start" onClick={this.toggleRunning}>
+            <i className={`fa fa-${startButton}`} aria-hidden="true"></i>
+          </button>
+        </div>
+        <div className="soundboard-instruments">
+          <VibraphoneBoard currentBeat={this.state.currentBeat} mm={this.state.mm} />
+          <DrumsBoard currentBeat={this.state.currentBeat} mm={this.state.mm} />
+        </div>
       </div>
     );
   }
