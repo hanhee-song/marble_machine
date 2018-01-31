d6f9627673eaa7aa0618a90e12c6647346605faf
Hanhee Song <song.hanhee@gmail.com>
2018-01-30 22:48:29 -0500

    add functional but unstyled slider for tempo

--- src/components/soundboard.jsx
+++ src/components/soundboard.jsx
@@ -7,45 +7,57 @@ class Soundboard extends React.Component {
     super(props);
     this.state = {
       currentBeat: -1,
-      interval: null,
+      timeout: null,
       tempo: 102,
       mm: 128,
     };
     this.handlePause = this.handlePause.bind(this);
     this.handleResume = this.handleResume.bind(this);
     this.toggleRunning = this.toggleRunning.bind(this);
+    this.handleTempoChange = this.handleTempoChange.bind(this);
   }
     
   handleResume() {
-    const interval = setInterval(() => {
+    const timeoutCallback = () => {
       const nextBeat = this.state.currentBeat + 1 === this.state.mm
       ? 0 : this.state.currentBeat + 1;
       this.setState({ currentBeat: nextBeat });
-    }, this.state.tempo);
-    this.setState({ interval });
+      const timeout = setTimeout(timeoutCallback, this.state.tempo);
+      this.setState({ timeout });
+    };
+    timeoutCallback();
+    // const timeout = setTimeout(timeoutCallback, this.state.tempo);
+    // this.setState({ timeout });
   }
   
   handlePause() {
-    clearInterval(this.state.interval);
-    this.setState({ interval: null });
+    clearTimeout(this.state.timeout);
+    this.setState({ timeout: null });
   }
   
   toggleRunning() {
-    if (this.state.interval) {
+    if (this.state.timeout) {
       this.handlePause();
     } else {
       this.handleResume();
     }
   }
   
+  handleTempoChange(e) {
+    this.setState({ tempo: e.target.value });
+  }
+  
   render () {
-    const startButton = this.state.interval ? "pause" : "play";
+    const startButton = this.state.timeout ? "pause" : "play";
     return (
       <div className="soundboard">
         <div className="soundboard-controls">
           <button className="soundboard-controls-start" onClick={this.toggleRunning}>
             <i className={`fa fa-${startButton}`} aria-hidden="true"></i>
           </button>
+          <input type="range" min="30" max="180"
+            value={this.state.tempo}
+            onChange={this.handleTempoChange}/>
         </div>
         <div className="soundboard-instruments">
           <VibraphoneBoard currentBeat={this.state.currentBeat} mm={this.state.mm} />
