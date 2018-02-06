c866d03e273dd11dfab75e2a6cea6f127e13e069
Hanhee Song <song.hanhee@gmail.com>
2018-02-06 15:56:34 -0500

    fix content warning for footer

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
         <div className="footer">
           Hanhee Song
           <div className="about-me">
-            <a href="https://hanhee-song.com" rel="noopener noreferrer" target="_blank"><i className="fa fa-home" aria-hidden="true"></i></a>
-            <a href="https://github.com/hanhee-song/marble_machine" rel="noopener noreferrer" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
-            <a href="https://www.linkedin.com/in/hanhee-song" rel="noopener noreferrer" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
+            <a href="https://hanhee-song.com" rel="noopener noreferrer" target="_blank"><i className="fa fa-home" aria-hidden="true"></i>&nbsp;Portfolio</a>
+            <a href="https://github.com/hanhee-song/marble_machine" rel="noopener noreferrer" target="_blank"><i className="fa fa-github" aria-hidden="true"></i>&nbsp;Github</a>
+            <a href="https://www.linkedin.com/in/hanhee-song" rel="noopener noreferrer" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i>&nbsp;LinkedIn</a>
             <a href="mailto:song.hanhee@gmail.com"><i className="fa fa-envelope" aria-hidden="true"></i>&nbsp;<div className="text">song.hanhee@gmail.com</div> </a>
           </div>
         </div>
