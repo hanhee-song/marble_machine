f2d060f23e76d914eba3a92c0e0a0f62a0f4df36
Hanhee Song <song.hanhee@gmail.com>
2018-02-08 11:13:17 -0500

    add more notes

--- src/classes/vibraphone.js
+++ src/classes/vibraphone.js
@@ -10,8 +10,13 @@ class Vibraphone extends Instrument {
   }
   
   setup() {
-    this.notes = ["b2", "c2", "d2", "e2",
-    "fs2", "g3", "a3", "b3", "c3", "d3", "e3"].reverse();
+    this.notes = [
+      "0e", "0fs",
+      "1g", "1a", "1b", "1c", "1d", "1e", "1fs",
+      "2g", "2a", "2b", "2c", "2d", "2e", "2fs",
+      "3g", "3a", "3b", "3c", "3d", "3e", "3fs",
+      "4g"
+    ].reverse();
     this.notes.forEach((note) => {
       this.sounds[note] = new Audio(`audio/vib_${note}.wav`);
       this.sounds[note].url = `public/audio/vib_${note}.wav`;
@@ -20,79 +25,79 @@ class Vibraphone extends Instrument {
   }
   
   initializeMarble() {
-    // this.addNote("e3", 0);
-    this.addNote("e2", 4);
-    this.addNote("b2", 4);
-    this.addNote("b3", 6);
-    this.addNote("e2", 12);
-    this.addNote("b2", 12);
-    this.addNote("a3", 14);
+    // this.addNote("3e", 0);
+    this.addNote("2e", 4);
+    this.addNote("2b", 4);
+    this.addNote("3b", 6);
+    this.addNote("2e", 12);
+    this.addNote("2b", 12);
+    this.addNote("3a", 14);
     
-    this.addNote("g3", 16);
-    this.addNote("a3", 18);
-    this.addNote("e2", 20);
-    this.addNote("b2", 20);
-    this.addNote("b3", 22);
-    this.addNote("g3", 26);
-    this.addNote("a3", 28);
-    this.addNote("e2", 28);
-    this.addNote("b2", 28);
-    this.addNote("d3", 30);
+    this.addNote("3g", 16);
+    this.addNote("3a", 18);
+    this.addNote("2e", 20);
+    this.addNote("2b", 20);
+    this.addNote("3b", 22);
+    this.addNote("3g", 26);
+    this.addNote("3a", 28);
+    this.addNote("2e", 28);
+    this.addNote("2b", 28);
+    this.addNote("3d", 30);
     
-    this.addNote("d2", 36);
-    this.addNote("b2", 36);
-    this.addNote("b3", 38);
-    this.addNote("d2", 44);
-    this.addNote("b2", 44);
-    this.addNote("a3", 46);
+    this.addNote("2d", 36);
+    this.addNote("2b", 36);
+    this.addNote("3b", 38);
+    this.addNote("2d", 44);
+    this.addNote("2b", 44);
+    this.addNote("3a", 46);
     
-    this.addNote("g3", 48);
-    this.addNote("a3", 50);
-    this.addNote("d2", 52);
-    this.addNote("b2", 52);
-    this.addNote("fs2", 54);
-    this.addNote("g3", 58);
-    this.addNote("a3", 60);
-    this.addNote("d2", 60);
-    this.addNote("b2", 60);
-    this.addNote("d3", 62);
+    this.addNote("3g", 48);
+    this.addNote("3a", 50);
+    this.addNote("2d", 52);
+    this.addNote("2b", 52);
+    this.addNote("2fs", 54);
+    this.addNote("3g", 58);
+    this.addNote("3a", 60);
+    this.addNote("2d", 60);
+    this.addNote("2b", 60);
+    this.addNote("3d", 62);
     
-    this.addNote("fs2", 68);
-    this.addNote("d2", 68);
-    this.addNote("b2", 68);
-    this.addNote("b3", 70);
-    this.addNote("fs2", 76);
-    this.addNote("d2", 76);
-    this.addNote("b2", 76);
-    this.addNote("d3", 78);
+    this.addNote("2fs", 68);
+    this.addNote("2d", 68);
+    this.addNote("2b", 68);
+    this.addNote("3b", 70);
+    this.addNote("2fs", 76);
+    this.addNote("2d", 76);
+    this.addNote("2b", 76);
+    this.addNote("3d", 78);
     
-    this.addNote("c3", 80);
-    this.addNote("b3", 82);
-    this.addNote("fs2", 84);
-    this.addNote("d2", 84);
-    this.addNote("b2", 84);
-    this.addNote("a3", 86);
-    this.addNote("g3", 90);
-    this.addNote("a3", 92);
-    this.addNote("d2", 92);
-    this.addNote("b2", 92);
-    this.addNote("e2", 94);
+    this.addNote("3c", 80);
+    this.addNote("3b", 82);
+    this.addNote("2fs", 84);
+    this.addNote("2d", 84);
+    this.addNote("2b", 84);
+    this.addNote("3a", 86);
+    this.addNote("3g", 90);
+    this.addNote("3a", 92);
+    this.addNote("2d", 92);
+    this.addNote("2b", 92);
+    this.addNote("2e", 94);
     
-    this.addNote("c2", 98);
-    this.addNote("e2", 100);
-    this.addNote("b3", 102);
-    this.addNote("b2", 104);
-    this.addNote("c2", 106);
-    this.addNote("d2", 108);
-    this.addNote("d3", 110);
+    this.addNote("2c", 98);
+    this.addNote("2e", 100);
+    this.addNote("3b", 102);
+    this.addNote("2b", 104);
+    this.addNote("2c", 106);
+    this.addNote("2d", 108);
+    this.addNote("3d", 110);
     
-    this.addNote("c3", 112);
-    this.addNote("b3", 114);
-    this.addNote("d2", 116);
-    this.addNote("a3", 118);
-    this.addNote("g3", 122);
-    this.addNote("a3", 124);
-    this.addNote("e3", 126);
+    this.addNote("3c", 112);
+    this.addNote("3b", 114);
+    this.addNote("2d", 116);
+    this.addNote("3a", 118);
+    this.addNote("3g", 122);
+    this.addNote("3a", 124);
+    this.addNote("3e", 126);
   }
 }
 
