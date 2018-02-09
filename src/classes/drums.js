fb3b97bdc70e5a4370e223a45083d721a940887b
Hanhee Song <song.hanhee@gmail.com>
2018-02-09 16:25:31 -0500

    remove no-longer-necessary initialization code

--- src/classes/drums.js
+++ src/classes/drums.js
@@ -5,8 +5,6 @@ class Drums extends Instrument {
     super(props);
     // this.notes;
     this.setup();
-    // this.initializeMarble();
-    // this._preloadAudio();
     this.name = "Drums";
   }
   
@@ -19,69 +17,6 @@ class Drums extends Instrument {
     this.sounds["snare"].volume = 0.27;
     this.sounds["hat"].volume = 0.12;
     this.sounds["crash"].volume= 0.2;
-  }
-  
-  initializeMarble() {
-    for (let i = 0; i < 12; i++) {
-      this.addNote("snare", (8 * i + 4) / 2);
-    }
-    this.addNote("snare", 104 / 2);
-    this.addNote("snare", 110 / 2);
-    this.addNote("snare", 114 / 2);
-    this.addNote("snare", 120 / 2);
-    this.addNote("snare", 126 / 2);
-    
-    this.addNote("kick", 0 / 2);
-    this.addNote("kick", 8 / 2);
-    this.addNote("kick", 16 / 2);
-    this.addNote("kick", 22 / 2);
-    this.addNote("kick", 26 / 2);
-    
-    this.addNote("kick", 32 / 2);
-    this.addNote("kick", 40 / 2);
-    this.addNote("kick", 48 / 2);
-    this.addNote("kick", 58 / 2);
-    this.addNote("kick", 62 / 2);
-    
-    this.addNote("kick", 64 / 2);
-    this.addNote("kick", 72 / 2);
-    this.addNote("kick", 80 / 2);
-    this.addNote("kick", 82 / 2);
-    this.addNote("kick", 86 / 2);
-    this.addNote("kick", 90 / 2);
-    
-    this.addNote("kick", 96 / 2);
-    this.addNote("kick", 100 / 2);
-    this.addNote("kick", 110 / 2);
-    this.addNote("kick", 116 / 2);
-    this.addNote("kick", 122 / 2);
-    // this.addNote("kick", 126 / 2);
-    
-    this.addNote("hat", 2 / 2);
-    this.addNote("hat", 8 / 2);
-    this.addNote("hat", 16 / 2);
-    this.addNote("hat", 24 / 2);
-    this.addNote("hat", 28 / 2);
-    
-    this.addNote("hat", (32 + 2) / 2);
-    this.addNote("hat", (32 + 8) / 2);
-    this.addNote("hat", (32 + 16) / 2);
-    this.addNote("hat", (32 + 24) / 2);
-    this.addNote("hat", (32 + 30) / 2);
-    
-    this.addNote("hat", 66 / 2);
-    this.addNote("hat", 72 / 2);
-    this.addNote("hat", 82 / 2);
-    this.addNote("hat", 86 / 2);
-    this.addNote("hat", 90 / 2);
-    this.addNote("hat", 94 / 2);
-    
-    this.addNote("hat", 100 / 2);
-    this.addNote("hat", 108 / 2);
-    this.addNote("hat", 120 / 2);
-    this.addNote("hat", 124 / 2);
-    
-    // this.addNote("crash", 0 / 2);
   }
 }
 
--- src/classes/vibraphone.js
+++ src/classes/vibraphone.js
@@ -3,10 +3,8 @@ import Instrument from './instrument';
 class Vibraphone extends Instrument {
   constructor(props) {
     super(props);
-    // this.notes
+    // this.notesl
     this.setup();
-    // this.initializeMarble();
-    // this._preloadAudio();
     this.name = "Vibraphone";
   }
   
@@ -24,82 +22,6 @@ class Vibraphone extends Instrument {
       this.sounds[note].volume = 0.16;
     });
   }
-  
-  // initializeMarble() {
-  //   // this.addNote("3e", 0 / 2);
-  //   this.addNote("2e", 4 / 2);
-  //   this.addNote("2b", 4 / 2);
-  //   this.addNote("3b", 6 / 2);
-  //   this.addNote("2e", 12 / 2);
-  //   this.addNote("2b", 12 / 2);
-  //   this.addNote("3a", 14 / 2);
-  //
-  //   this.addNote("3g", 16 / 2);
-  //   this.addNote("3a", 18 / 2);
-  //   this.addNote("2e", 20 / 2);
-  //   this.addNote("2b", 20 / 2);
-  //   this.addNote("3b", 22 / 2);
-  //   this.addNote("3g", 26 / 2);
-  //   this.addNote("3a", 28 / 2);
-  //   this.addNote("2e", 28 / 2);
-  //   this.addNote("2b", 28 / 2);
-  //   this.addNote("3d", 30 / 2);
-  //
-  //   this.addNote("2d", 36 / 2);
-  //   this.addNote("2b", 36 / 2);
-  //   this.addNote("3b", 38 / 2);
-  //   this.addNote("2d", 44 / 2);
-  //   this.addNote("2b", 44 / 2);
-  //   this.addNote("3a", 46 / 2);
-  //
-  //   this.addNote("3g", 48 / 2);
-  //   this.addNote("3a", 50 / 2);
-  //   this.addNote("2d", 52 / 2);
-  //   this.addNote("2b", 52 / 2);
-  //   this.addNote("2fs", 54 / 2);
-  //   this.addNote("3g", 58 / 2);
-  //   this.addNote("3a", 60 / 2);
-  //   this.addNote("2d", 60 / 2);
-  //   this.addNote("2b", 60 / 2);
-  //   this.addNote("3d", 62 / 2);
-  //
-  //   this.addNote("2fs", 68 / 2);
-  //   this.addNote("2d", 68 / 2);
-  //   this.addNote("2b", 68 / 2);
-  //   this.addNote("3b", 70 / 2);
-  //   this.addNote("2fs", 76 / 2);
-  //   this.addNote("2d", 76 / 2);
-  //   this.addNote("2b", 76 / 2);
-  //   this.addNote("3d", 78 / 2);
-  //
-  //   this.addNote("3c", 80 / 2);
-  //   this.addNote("3b", 82 / 2);
-  //   this.addNote("2fs", 84 / 2);
-  //   this.addNote("2d", 84 / 2);
-  //   this.addNote("2b", 84 / 2);
-  //   this.addNote("3a", 86 / 2);
-  //   this.addNote("3g", 90 / 2);
-  //   this.addNote("3a", 92 / 2);
-  //   this.addNote("2d", 92 / 2);
-  //   this.addNote("2b", 92 / 2);
-  //   this.addNote("2e", 94 / 2);
-  //
-  //   this.addNote("2c", 98 / 2);
-  //   this.addNote("2e", 100 / 2);
-  //   this.addNote("3b", 102 / 2);
-  //   this.addNote("2b", 104 / 2);
-  //   this.addNote("2c", 106 / 2);
-  //   this.addNote("2d", 108 / 2);
-  //   this.addNote("3d", 110 / 2);
-  //
-  //   this.addNote("3c", 112 / 2);
-  //   this.addNote("3b", 114 / 2);
-  //   this.addNote("2d", 116 / 2);
-  //   this.addNote("3a", 118 / 2);
-  //   this.addNote("3g", 122 / 2);
-  //   this.addNote("3a", 124 / 2);
-  //   this.addNote("3e", 126 / 2);
-  // }
 }
 
 export default Vibraphone;
