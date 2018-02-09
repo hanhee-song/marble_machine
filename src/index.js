93b5e682b58210211349e51bd9ad90c7aa6fffd2
Hanhee Song <song.hanhee@gmail.com>
2018-02-09 11:26:54 -0500

    wrap app in hashrouter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
