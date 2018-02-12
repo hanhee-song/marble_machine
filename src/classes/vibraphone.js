fd0558ff10963c9fce85c694fadeb4e824192002
Hanhee Song <song.hanhee@gmail.com>
2018-02-11 20:24:06 -0500

    add three more notes to top of register

--- src/classes/vibraphone.js
+++ src/classes/vibraphone.js
@@ -15,7 +15,7 @@ class Vibraphone extends Instrument {
       "1g", "1a", "1b", "1c", "1d", "1e", "1fs",
       "2g", "2a", "2b", "2c", "2d", "2e", "2fs",
       "3g", "3a", "3b", "3c", "3d", "3e", "3fs",
-      "4g"
+      "4g", "4a", "4b", "4c"
     ].reverse();
     this.notes.forEach((note) => {
       this.sounds[note] = new Audio(`audio/vib_${note}.wav`);
